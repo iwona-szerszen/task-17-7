2482151e538342d19b42f76a9c0b657c8d194426
Iwona Szerszeń <iwona.szerszen@outlook.com>
2018-10-07 13:57:03 +0200

    Add project

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '191181348335-nrbb5ooat2ld0i8tvhl3pivgpddaescc.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': 'Bm3WLLeS-wjjI2BV7xZm61OS',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
